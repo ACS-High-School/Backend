5cb8f7dafac0c648d4aaab7cb10b886b52d6aff5
hongjunpyo <ejhjjp11@naver.com>
2024-02-06 05:12:30 +0900

    chore : 패캐지 이름 수정

---
+++ src/test/java/acs/b3o/B3oApplicationTests.java
@@ -0,0 +1,13 @@
+package acs.b3o;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B3oApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
