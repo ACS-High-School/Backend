a4740062e27d9c5c5b3584c3da3ed6e59184e785
hongjunpyo <ejhjjp11@naver.com>
2024-02-06 05:40:13 +0900

    docs : Swagger 명세 추가
    - S3Controller 관련 api 명세 작성

--- src/main/java/acs/b3o/controller/S3Controller.java
+++ src/main/java/acs/b3o/controller/S3Controller.java
@@ -2,6 +2,8 @@ package acs.b3o.controller;
 
 
 import acs.b3o.service.S3Service;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.io.IOException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+@Tag(name=" S3 컨트롤러", description = "s3 api")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/inference")
@@ -20,6 +23,7 @@ public class S3Controller {
 
   private final S3Service awsS3Service;
 
+  @Operation(summary = "파일 업로드", description = "s3에 파일을 업로드 합니다.")
   @PostMapping("/uploadFile")
   public ResponseEntity<List<String>> uploadFile(
       @RequestParam("multipartFiles") List<MultipartFile> multipartFiles,
@@ -28,6 +32,7 @@ public class S3Controller {
     return ResponseEntity.ok(awsS3Service.uploadFile(multipartFiles, subFolderPath, taskTitle));
   }
 
+  @Operation(summary = "파일 다운로드", description = "s3 파일을 다운로드 합니다.")
   @GetMapping ("/getFile")
   public ResponseEntity<byte[]> download() throws IOException {
     return awsS3Service.getFile("data/raw-data/test_data.csv");
