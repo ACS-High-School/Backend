6bc81d379b74199d7a98791c006e383df8c13159
sw801733 <sw801733@naver.com>
2024-02-14 01:07:23 +0900

    fix : CORS 에러를 위한 SecurityConfig 수정
    CORS 허용을 SecurityConfig 에도 추가 필요

--- src/main/java/acs/b3o/config/SecurityConfig.java
+++ src/main/java/acs/b3o/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package acs.b3o.config;
 
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Collections;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +14,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -28,6 +32,18 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf(Customizer.withDefaults()) // 이렇게 변경합니다.
+            .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+                @Override
+                public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                    config.setAllowedMethods(Collections.singletonList("*"));
+                    config.setAllowCredentials(true);
+                    config.setAllowedHeaders(Collections.singletonList("*"));
+                    config.setMaxAge(3600L); //1시간
+                    return config;
+                }
+            }))
             .authorizeRequests(authorizeRequests ->
                 authorizeRequests
                     .requestMatchers("/").permitAll() // 인증 없이 접근 허용
@@ -40,7 +56,7 @@ public class SecurityConfig {
 
     @Bean
     public JwtDecoder jwtDecoder() {
-        String uri = "";
+        String uri = "https://cognito-idp.ap-northeast-2.amazonaws.com/ap-northeast-2_5lakKmsd5/.well-known/jwks.json";
 
         NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.withJwkSetUri(uri).build();
         return jwtDecoder;
