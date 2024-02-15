e5babf0b143b418dd8ce8870ae7e36208bb6c68b
sw801733 <sw801733@naver.com>
2024-02-16 00:37:23 +0900

    fix : csrf disable 추가
    - csrf 토큰 확인으로 인한 오류 해결을 위해 disable 추가

--- src/main/java/acs/b3o/config/SecurityConfig.java
+++ src/main/java/acs/b3o/config/SecurityConfig.java
@@ -11,11 +11,13 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
 
 @Configuration
 @EnableWebSecurity
@@ -31,7 +33,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(Customizer.withDefaults()) // 이렇게 변경합니다.
+            .csrf(AbstractHttpConfigurer::disable) // 이렇게 변경합니다.
             .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
                 @Override
                 public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
