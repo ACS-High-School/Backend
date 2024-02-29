181c2edffc9613ca8b3f1c78d55f758cd51ab257
sw801733 <sw801733@naver.com>
2024-02-29 11:54:27 +0900

    AHSK-205 feat : UserGroup DTO 추가
    UserGroup 에 대한 Request 와 Response 추가

---
+++ src/main/java/acs/b3o/dto/request/UserGroupRequest.java
@@ -0,0 +1,12 @@
+package acs.b3o.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class UserGroupRequest {
+    private int groupcode;
+    private String user1;
+    private String user2;
+    private String user3;
+    private String user4;
+}
