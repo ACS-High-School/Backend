b92806eedb5386c62c91d9dc0f5341a004e95971
sw801733 <sw801733@naver.com>
2024-02-29 11:09:40 +0900

    AHSK-205 feat : UserGroup Entity 추가

---
+++ src/main/java/acs/b3o/entity/UserGroup.java
@@ -0,0 +1,42 @@
+package acs.b3o.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "usergroup")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserGroup {
+
+    @Id
+    private int groupcode; // 그룹 코드, 기본 키로 사용
+
+    @ManyToOne
+    @JoinColumn(name = "user1", referencedColumnName = "nickname")
+    private User user1; // user1 닉네임, User 엔티티와의 외래 키 관계
+
+    @ManyToOne
+    @JoinColumn(name = "user2", referencedColumnName = "nickname")
+    private User user2; // user2 닉네임, User 엔티티와의 외래 키 관계
+
+    @ManyToOne
+    @JoinColumn(name = "user3", referencedColumnName = "nickname")
+    private User user3; // user3 닉네임, User 엔티티와의 외래 키 관계
+
+    @ManyToOne
+    @JoinColumn(name = "user4", referencedColumnName = "nickname")
+    private User user4; // user4 닉네임, User 엔티티와의 외래 키 관계
+
+}
