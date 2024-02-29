7dd32e922a69b71e0c08dc697892789413a48353
sw801733 <sw801733@naver.com>
2024-02-29 12:00:06 +0900

    AHSK-205 refact : FederatedResponse 클래스 이름 변경

--- src/main/java/acs/b3o/dto/response/FederatedResponse.java
+++ src/main/java/acs/b3o/dto/response/FederatedResponse.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 
 @Getter
 @Builder
-public class FederatedReponse {
+public class FederatedResponse {
     private String userInput1;
     private String userInput2;
     private String userInput3;
