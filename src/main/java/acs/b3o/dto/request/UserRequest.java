eb3b3e594915023e846b2b4cc73f1ab17c3404a6
sw801733 <sw801733@naver.com>
2024-02-29 11:54:09 +0900

    AHSK-205 feat : User DTO 추가
    User 에 대한 Request 와 Response 추가

---
+++ src/main/java/acs/b3o/dto/request/UserRequest.java
@@ -0,0 +1,11 @@
+package acs.b3o.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class UserRequest {
+    private String nickname;
+    private String name;
+    private String email;
+    private String company;
+}
