5ccbafbe2d79be3694e3b3557b2fd10427422d9f
sw801733 <sw801733@naver.com>
2024-03-02 02:20:28 +0900

    AHSK-217 fix : UserGroup 의 그룹 생성 정보 저장을 위한 로직 수정

--- src/main/java/acs/b3o/service/UserGroupService.java
+++ src/main/java/acs/b3o/service/UserGroupService.java
@@ -1,24 +1,35 @@
 package acs.b3o.service;
 
 import acs.b3o.dto.request.UserGroupRequest;
+import acs.b3o.entity.User;
 import acs.b3o.entity.UserGroup;
 import acs.b3o.repository.UserGroupRepository;
+import acs.b3o.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Transactional
 public class UserGroupService {
 
     @Autowired
     private UserGroupRepository userGroupRepository;
 
-    public void createUserGroup(UserGroupRequest userGroupRequest) {
+    @Autowired
+    private UserRepository userRepository;
+
+    public void createUserGroup(UserGroupRequest userGroupRequest, String username) {
         // UserGroup 엔티티 생성 및 설정
+
+        User user = userRepository.findByNickname(username);
+
         UserGroup userGroup = new UserGroup();
         userGroup.setGroupCode(userGroupRequest.getGroupCode());
-        // 유저 정보 설정, 예를 들어 userGroup.setUser1(username); 등
-        // ...
-//        userGroup.setUser1(username);
+        System.out.println(userGroupRequest.getGroupCode());
+        userGroup.setUser1(user);
+
+        System.out.println("username : " + username);
         userGroupRepository.save(userGroup); // 데이터베이스에 저장
     }
 }
