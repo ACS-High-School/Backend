26ca6cf64aa66b60adfa660beeaab9384a8acbd2
sw801733 <sw801733@naver.com>
2024-03-02 02:18:12 +0900

    AHSK-217 feat : UserRepository에 findByNickname 메서드 추가

--- src/main/java/acs/b3o/repository/UserRepository.java
+++ src/main/java/acs/b3o/repository/UserRepository.java
@@ -4,4 +4,6 @@ import acs.b3o.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, String> {
+
+    User findByNickname(String username);
 }
