ee0f797b6cedeab5f3752d225d619b985eaaff01
sw801733 <sw801733@naver.com>
2024-03-02 16:51:20 +0900

    AHSK-217 feat : UserGroupService 에 joinUserGroup 메서드 추가
    그룹 참여를 위해 그룹코드를 입력한 유저를 그룹에 Update 하기 위한 메서드

--- src/main/java/acs/b3o/service/UserGroupService.java
+++ src/main/java/acs/b3o/service/UserGroupService.java
@@ -1,6 +1,7 @@
 package acs.b3o.service;
 
 import acs.b3o.dto.request.UserGroupRequest;
+import acs.b3o.dto.response.UserGroupResponse;
 import acs.b3o.entity.User;
 import acs.b3o.entity.UserGroup;
 import acs.b3o.repository.UserGroupRepository;
@@ -19,18 +20,50 @@ public class UserGroupService {
     @Autowired
     private UserRepository userRepository;
 
-    public void createUserGroup(UserGroupRequest userGroupRequest, String username) {
-        // UserGroup 엔티티 생성 및 설정
-
+    public UserGroupResponse createUserGroup(UserGroupRequest userGroupRequest, String username) {
         User user = userRepository.findByNickname(username);
 
         UserGroup userGroup = new UserGroup();
         userGroup.setGroupCode(userGroupRequest.getGroupCode());
-        System.out.println(userGroupRequest.getGroupCode());
         userGroup.setUser1(user);
 
-        System.out.println("username : " + username);
-        userGroupRepository.save(userGroup); // 데이터베이스에 저장
+        userGroupRepository.save(userGroup);
+
+        return UserGroupResponse.builder()
+            .groupCode(userGroupRequest.getGroupCode())
+            .user1(username)
+            .build();
+    }
+
+    public UserGroupResponse joinUserGroup(UserGroupRequest userGroupRequest, String username) {
+        User user = userRepository.findByNickname(username);
+
+        UserGroup userGroup = userGroupRepository.findByGroupCode(userGroupRequest.getGroupCode());
+
+        String message;
+
+        if (userGroup.getUser2() == null) {
+            userGroup.setUser2(user);
+            message = "사용자가 그룹에 성공적으로 추가되었습니다.";
+        } else if (userGroup.getUser3() == null) {
+            userGroup.setUser3(user);
+            message = "사용자가 그룹에 성공적으로 추가되었습니다.";
+        } else if (userGroup.getUser4() == null) {
+            userGroup.setUser4(user);
+            message = "사용자가 그룹에 성공적으로 추가되었습니다.";
+        } else {
+            message = "이 그룹은 이미 가득 찼습니다.";
+        }
+
+        userGroupRepository.save(userGroup);
+
+        return UserGroupResponse.builder()
+            .groupCode(userGroupRequest.getGroupCode())
+            .user1(userGroup.getUser1() != null ? userGroup.getUser1().getNickname() : null)
+            .user2(userGroup.getUser2() != null ? userGroup.getUser2().getNickname() : null)
+            .user3(userGroup.getUser3() != null ? userGroup.getUser3().getNickname() : null)
+            .user4(userGroup.getUser4() != null ? userGroup.getUser4().getNickname() : null)
+            .message(message)
+            .build();
     }
 }
-
