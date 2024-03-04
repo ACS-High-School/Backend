e2d1a47c7548626d9567c7d430457731388df69e
sw801733 <sw801733@naver.com>
2024-03-05 00:59:16 +0900

    AHSK-218 feat : GroupStatusRepository 생성

---
+++ src/main/java/acs/b3o/repository/GroupStatusRepository.java
@@ -0,0 +1,7 @@
+package acs.b3o.repository;
+
+import acs.b3o.entity.GroupStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupStatusRepository extends JpaRepository<GroupStatus, Integer> {
+}
