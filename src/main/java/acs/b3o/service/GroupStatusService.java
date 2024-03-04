d4dd2e88b8c16816578d57841680f6092908afa9
sw801733 <sw801733@naver.com>
2024-03-05 01:03:11 +0900

    AHSK-218 feat : GroupStatusService 생성 및 업로드 확인 요청 처리 메서드 구현

---
+++ src/main/java/acs/b3o/service/GroupStatusService.java
@@ -0,0 +1,20 @@
+package acs.b3o.service;
+
+import acs.b3o.entity.GroupStatus;
+import acs.b3o.repository.GroupStatusRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GroupStatusService {
+
+    @Autowired
+    private GroupStatusRepository repository;
+
+    public void confirmUpload(Integer groupCode) {
+        GroupStatus groupStatus = repository.findById(groupCode)
+            .orElseThrow(() -> new RuntimeException("Group not found"));
+        groupStatus.setUser1Confirmed(true); // 예시로 user1Confirmed만 업데이트. 실제 로직에 맞게 조정 필요
+        repository.save(groupStatus);
+    }
+}
