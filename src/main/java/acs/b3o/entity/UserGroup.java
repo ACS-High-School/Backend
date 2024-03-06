104f5e43f48df4c9d9581f9101b16f0115919a16
sw801733 <sw801733@naver.com>
2024-03-07 02:21:54 +0900

    AHSK-245 fix : UserGroup Entity 수정

--- src/main/java/acs/b3o/entity/UserGroup.java
+++ src/main/java/acs/b3o/entity/UserGroup.java
@@ -2,8 +2,6 @@ package acs.b3o.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -28,19 +26,19 @@ public class UserGroup {
     private int groupCode; // 그룹 코드, 기본 키로 사용
 
     @ManyToOne
-    @JoinColumn(name = "user1", referencedColumnName = "nickname")
-    private User user1; // user1 닉네임, User 엔티티와의 외래 키 관계
+    @JoinColumn(name = "user1", referencedColumnName = "username")
+    private User user1; // user1 username, User 엔티티와의 외래 키 관계
 
     @ManyToOne
-    @JoinColumn(name = "user2", referencedColumnName = "nickname")
-    private User user2; // user2 닉네임, User 엔티티와의 외래 키 관계
+    @JoinColumn(name = "user2", referencedColumnName = "username")
+    private User user2; // user2 username, User 엔티티와의 외래 키 관계
 
     @ManyToOne
-    @JoinColumn(name = "user3", referencedColumnName = "nickname")
-    private User user3; // user3 닉네임, User 엔티티와의 외래 키 관계
+    @JoinColumn(name = "user3", referencedColumnName = "username")
+    private User user3; // user3 username, User 엔티티와의 외래 키 관계
 
     @ManyToOne
-    @JoinColumn(name = "user4", referencedColumnName = "nickname")
-    private User user4; // user4 닉네임, User 엔티티와의 외래 키 관계
+    @JoinColumn(name = "user4", referencedColumnName = "username")
+    private User user4; // user4 username, User 엔티티와의 외래 키 관계
 
 }
