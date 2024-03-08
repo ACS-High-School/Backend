60f26316ba4c6b249d78a3e248f63d7e7cff6f8e
sw801733 <sw801733@naver.com>
2024-03-08 15:42:31 +0900

    Revert "AHSK-255 fix : 테이블 변경으로 User DTO 추가"
    This reverts commit 45362f349b6f44852a5f3d8ff9a0574ae70ec900.

--- src/main/java/acs/b3o/dto/request/UserRequest.java
+++ src/main/java/acs/b3o/dto/request/UserRequest.java
@@ -1,10 +1,8 @@
 package acs.b3o.dto.request;
 
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class UserRequest {
     private String username;
     private String email;
