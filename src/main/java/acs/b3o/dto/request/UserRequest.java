b616f7156326153eb104af44e723762e25787065
sw801733 <sw801733@naver.com>
2024-03-08 15:42:45 +0900

    AHSK-255 fix : 테이블 변경으로 User DTO 변경

--- src/main/java/acs/b3o/dto/request/UserRequest.java
+++ src/main/java/acs/b3o/dto/request/UserRequest.java
@@ -1,8 +1,10 @@
 package acs.b3o.dto.request;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class UserRequest {
     private String username;
     private String email;
