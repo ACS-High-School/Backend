9ee1050d54d048facd57b36164c48496ba4d34b3
hongjunpyo <ejhjjp11@naver.com>
2024-03-11 03:32:13 +0900

    AHSK-242 refact : InferenceRepository.java 사용하지 않는 매서드 삭제

--- src/main/java/acs/b3o/repository/InferenceRepository.java
+++ src/main/java/acs/b3o/repository/InferenceRepository.java
@@ -3,6 +3,7 @@ package acs.b3o.repository;
 import acs.b3o.entity.Inference;
 import acs.b3o.entity.User;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InferenceRepository extends JpaRepository<Inference, Integer> {
