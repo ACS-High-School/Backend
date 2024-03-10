0397ab8f9e23c731e58d54dc5c6838e097f78ea6
hongjunpyo <ejhjjp11@naver.com>
2024-03-11 03:48:46 +0900

    AHSK-242 feat : InferenceService.java Id 값을 통해 데이터 조회 기능 추가, Response result 값 추가

--- src/main/java/acs/b3o/service/InferenceService.java
+++ src/main/java/acs/b3o/service/InferenceService.java
@@ -9,6 +9,7 @@ import acs.b3o.repository.UserRepository;
 import jakarta.transaction.Transactional;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -29,7 +30,7 @@ public class InferenceService {
     Inference inference = new Inference();
     inference.setModel(model);
     inference.setTitle(title);
-    inference.setInput(title + "_x1");
+    inference.setInput(username + "-" + title + "-" + model + "-input.csv");
     inference.setStats("start");
     inference.setDate(new Date());
     inference.setUser(user);
@@ -56,14 +57,23 @@ public class InferenceService {
     ).collect(Collectors.toList());
   }
 
+  public InferenceResponse getInferenceById(Integer id) {
+    Optional<Inference> optionalInference = inferenceRepository.findById(id);
+    Inference inference = optionalInference.get();
+    return buildResponse(inference, "Inference data가 조회되었습니다.");
+  }
+
+
   private InferenceResponse buildResponse(Inference inference, String message) {
     return InferenceResponse.builder()
+        ._id(inference.get_id())
         .model(inference.getModel())
         .title(inference.getTitle())
         .date(inference.getDate())
         .input(inference.getInput())
         .stats(inference.getStats())
         .user(inference.getUser())
+        .result(inference.getResult())
         .message(message)
         .build();
   }
