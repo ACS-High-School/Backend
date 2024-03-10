a22c60f5b89307f9707cabe59d26cd6ae65773d7
hongjunpyo <ejhjjp11@naver.com>
2024-03-11 03:50:25 +0900

    AHSK-242 feat : InferenceController.java id 값을 통해 테이블 조회 구현

--- src/main/java/acs/b3o/controller/InferenceController.java
+++ src/main/java/acs/b3o/controller/InferenceController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -36,5 +37,10 @@ public class InferenceController {
     return ResponseEntity.ok(responses);
   }
 
+  @GetMapping("/result/{id}")
+  public ResponseEntity<InferenceResponse> getInferenceById(@PathVariable("id") Integer id) {
+    InferenceResponse response = inferenceService.getInferenceById(id);
+    return ResponseEntity.ok(response);
+  }
 
 }
