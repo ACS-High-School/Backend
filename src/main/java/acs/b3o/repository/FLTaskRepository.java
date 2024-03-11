58db92a190b1743dd4edff47594061d2b2dcb520
sw801733 <sw801733@naver.com>
2024-03-11 22:25:59 +0900

    AHSK-256 feat : DynamoDB 테이블의 CRUD 를 위한 FLTaskRepository 생성 및 메서드 구현
    - 파티션 키와 정렬 키를 이용한 Read 를 위한 메서드 구현

--- src/main/java/acs/b3o/repository/FLTaskRepository.java
+++ src/main/java/acs/b3o/repository/FLTaskRepository.java
@@ -6,17 +6,24 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.Key;
 import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
 
 @Repository
 public class FLTaskRepository {
     private final DynamoDbTable<FLTask> FLTaskTable;
 
-    @Value("${dynamodb.table.name}")
-    private String dynamoDBTableName;
-    public FLTaskRepository(@Autowired DynamoDbEnhancedClient dynamoDbEnhancedClient) {
+    public FLTaskRepository(@Autowired DynamoDbEnhancedClient dynamoDbEnhancedClient,
+        @Value("${dynamodb.table.name}") String dynamoDBTableName) {
         this.FLTaskTable = dynamoDbEnhancedClient.table(dynamoDBTableName, TableSchema.fromBean(FLTask.class));
     }
+
+    public FLTask getTaskById(String taskName, String taskID) {
+        // 파티션 키와 정렬 키를 모두 사용하여 DynamoDB 테이블에서 항목을 가져옵니다.
+        Key key = Key.builder().partitionValue(taskName).sortValue(taskID).build();
+        return FLTaskTable.getItem(key);
+    }
+
 
 
 
