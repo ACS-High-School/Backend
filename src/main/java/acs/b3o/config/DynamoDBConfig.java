12003f8c31b882d245c6950b52e0987e597493bc
sw801733 <sw801733@naver.com>
2024-03-11 21:01:18 +0900

    AHSK-263 fix : DynamoDbEnhancedClient Bean 등록을 위한 @Configuration 추가
    - DynamoDbEnhancedClient 을 Bean 에 등록해서 사용하기 위한 Config 어노테이션 추가

--- src/main/java/acs/b3o/config/DynamoDBConfig.java
+++ src/main/java/acs/b3o/config/DynamoDBConfig.java
@@ -3,12 +3,14 @@ package acs.b3o.config;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
+@Configuration
 public class DynamoDBConfig {
     @Value("${cloud.aws.credentials.access-key}")
     private String accessKey;
