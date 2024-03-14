a0f720e537676b1cb3a4a398201774bb841e3333
sw801733 <sw801733@naver.com>
2024-03-14 20:01:37 +0900

    AHSK-279 feat : FederatedRepository 구현

---
+++ src/main/java/acs/b3o/repository/FederatedRepository.java
@@ -0,0 +1,8 @@
+package acs.b3o.repository;
+
+import acs.b3o.entity.Federated;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FederatedRepository extends JpaRepository<Federated, Integer> {
+
+}
