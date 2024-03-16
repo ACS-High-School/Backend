aa3487ed76e164d68c94b7ebfde0680094dc310c
sw801733 <sw801733@naver.com>
2024-03-16 23:22:05 +0900

    AHSK-278 feat : UserGroup 에서 user 로 find 메서드 추가

--- src/main/java/acs/b3o/repository/UserGroupRepository.java
+++ src/main/java/acs/b3o/repository/UserGroupRepository.java
@@ -1,8 +1,17 @@
 package acs.b3o.repository;
 
+import acs.b3o.entity.User;
 import acs.b3o.entity.UserGroup;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserGroupRepository extends JpaRepository<UserGroup, Long> {
     UserGroup findByGroupCode(int groupCode);
+
+
+    @Query("SELECT ug FROM UserGroup ug WHERE ug.user1 = :user OR ug.user2 = :user OR ug.user3 = :user OR ug.user4 = :user")
+    List<UserGroup> findByUser(@Param("user") User user);
+
 }
