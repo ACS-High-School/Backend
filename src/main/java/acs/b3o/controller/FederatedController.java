5e8902ebd6f32175e2f6407938778ad0ad9234dd
sw801733 <sw801733@naver.com>
2024-03-16 22:39:31 +0900

    AHSK-278 feat : FederatedController 생성

---
+++ src/main/java/acs/b3o/controller/FederatedController.java
@@ -0,0 +1,10 @@
+package acs.b3o.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/fl")
+public class FederatedController {
+
+}
